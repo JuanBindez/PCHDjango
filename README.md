b57bc4edb622f6b627c9dc98aa8dedb4dd4e5f94
JuanBindez <juanbindez780@gmail.com>
2023-07-26 16:02:21 -0300

    update 1.1-rc1 -> 1.1-rc2

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h1 align="">
-  <img alt="NextLevelWeek" title="#NextLevelWeek" src="CommerceCore_ico.png" width="150px"/> 
-   <h1 align="">PyCommerceHub 1.0.0</h1>
+  <img alt="NextLevelWeek" title="#NextLevelWeek" src="PyCommerceHub.png" width="150px"/> 
+   <h1 align="">PyCommerceHub 1.0-rc2</h1>
 </h1>
 
 
--- store/urls.py
+++ store/urls.py
@@ -28,13 +28,14 @@ from .views.login import Login , logout
 from .views.cart import Cart
 from .views.checkout import CheckOut
 from .views.orders import OrderView
+from .views.profile import profile_view
 from .middlewares.auth import  auth_middleware
 
 
 urlpatterns = [
     path('', Index.as_view(), name='homepage'),
     path('store', store , name='store'),
-
+    path('profile', profile_view , name='profile'),
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout , name='logout'),
